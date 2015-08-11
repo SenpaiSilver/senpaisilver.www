ca5b9c3822af6f92cbea47069a280b14f5eb40d8
SenpaiSilver <silvermesprims@gmail.com>
2015-08-11 19:17:11 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+senpaisilver.www
+================
+
+This repository represents the home page of [SenpaiSilver.com](http://www.senpaisilver.com).
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>SenpaiSilver</title>
+		
+		<link rel="stylesheet"  href="./media/style.css" />
+		
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=700, user-scalable=0" />
+	</head>
+	<body>
+		<div class="material-menu">
+			<div class="header">
+				<div class="profile-picture"></div>
+				<div class="title">
+					SenpaiSilver
+				</div>
+			</div>
+			<ul class="material-menuitems">
+				<li class="material-menuitems blog"><a href="http://blog.senpaisilver.com">Blog</a></li>
+				<li class="material-menuitems github"><a href="https://github.com/SenpaiSilver/">GitHub</a></li>
+				<li class="material-menuitems steam"><a href="http://steamcommunity.com/id/senpaisilver/">Steam Community</a></li>
+				<li class="material-menuitems googlep"><a href="https://plus.google.com/+SenpaiSilver/about">Google+</a></li>
+				<li class="material-menuitems twitter"><a href="https://twitter.com/SenpaiSilver">Twitter</a></li>
+				<li class="material-menuitems youtube"><a href="https://youtube.com/SenpaiSilver">YouTube</a></li>
+			</ul>
+		</div>
+		<div class="material-mainpage">
+			<h1>Projects</h1>
+			<ul class="cards">
+				<li class="card artifacthunter"><a href="https://github.com/SenpaiSilver/ArtifactHunter"><span>ArtifactHunter</span></a></li>
+				<li class="card fov"><a href="https://github.com/SenpaiSilver/FOVComparator"><span>FOVComparator</span></a></li>
+			</ul>
+		</div>
+	</body>
+</html>
