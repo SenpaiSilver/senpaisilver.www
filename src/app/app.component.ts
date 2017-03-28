3c266130d25b661630ae7f5bcdd30291d0f513fc
SenpaiSilver <silvermesprims@gmail.com>
2017-03-29 00:06:27 +0200

    First rework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  links = [
+    {title: "Blog", href: "http://blog.senpaisilver.com"},
+    {title: "GitHub", href: "https://github.com/SenpaiSilver"},
+    {title: "Steam", href: "http://steamcommunity.com/id/senpaisilver/"},
+    {title: "Google+", href: "https://plus.google.com/+SenpaiSilver/about"},
+    {title: "@SenpaiSilver", href: "https://twitter.com/SenpaiSilver"},
+    {title: "YouTube", href: "https://youtube.com/SenpaiSilver"},
+    {title: "EVE Online", href: "http://secure.eveonline.com/signup/?invc=503fc3ad-0989-4589-a826-e4b786948803&action=buddy"}
+  ];
+  /*projects = [
+    {title: "ArtifactHunter", href: "https://github.com/SenpaiSilver/ArtifactHunter"},
+    {title: "FOVComparator", href: "https://github.com/SenpaiSilver/FOVComparator"}
+  ]*/
 }
