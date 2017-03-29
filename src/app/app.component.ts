6cfd7cf0094b6c11fd27eb2f4ee1f5a930981ba9
SenpaiSilver <silvermesprims@gmail.com>
2017-03-29 04:12:08 +0200

    Populated the menu with links, icons must be generated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   links = [
-    {title: "Blog", href: "http://blog.senpaisilver.com"},
-    {title: "GitHub", href: "https://github.com/SenpaiSilver"},
-    {title: "Steam", href: "http://steamcommunity.com/id/senpaisilver/"},
-    {title: "Google+", href: "https://plus.google.com/+SenpaiSilver/about"},
-    {title: "@SenpaiSilver", href: "https://twitter.com/SenpaiSilver"},
-    {title: "YouTube", href: "https://youtube.com/SenpaiSilver"},
-    {title: "EVE Online", href: "http://secure.eveonline.com/signup/?invc=503fc3ad-0989-4589-a826-e4b786948803&action=buddy"}
+    new Links('Blog', 'http://blog.senpaisilver.com'),
+    new Links('GitHub', 'https://github.com/SenpaiSilver'),
+    new Links('Steam', 'http://steamcommunity.com/id/senpaisilver/'),
+    new Links('Google+', 'https://plus.google.com/+SenpaiSilver/about'),
+    new Links('Twitter', 'https://twitter.com/SenpaiSilver'),
+    new Links('YouTube', 'https://youtube.com/SenpaiSilver'),
+    new Links('EVE Online', 'http://secure.eveonline.com/signup/?invc=503fc3ad-0989-4589-a826-e4b786948803&action=buddy')
   ];
-  /*projects = [
-    {title: "ArtifactHunter", href: "https://github.com/SenpaiSilver/ArtifactHunter"},
-    {title: "FOVComparator", href: "https://github.com/SenpaiSilver/FOVComparator"}
-  ]*/
+}
+
+class Links {
+  public title: string;
+  public href: string;
+  public icon: string;
+
+  public constructor(title: string, href: string, icon = 'none') {
+    this.title = title;
+    this.href = href;
+    this.icon = icon;
+  }
 }
