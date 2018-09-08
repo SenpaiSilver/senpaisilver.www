44c34dcf5d962556485ef7f8289356526308b112
SenpaiSilver <silvermesprims@gmail.com>
2018-09-08 23:59:10 +0200

    Blank project has been setup

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 senpaisilver.www
 ================
 
-This repository represents the home page of [SenpaiSilver.com](http://www.senpaisilver.com).  
-It is now built with Angular.
+This repository represents the home page of [SenpaiSilver.com](http://www.senpaisilver.com).
---
+++ application/views/base/head.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>SenpaiSilver</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="<?=base_url('assets/css/main.css')?>" rel="stylesheet"/>
+</head>
+<body>
