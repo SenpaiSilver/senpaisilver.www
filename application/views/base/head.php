7f9204dfe8816646515ce494e61a1afe267d3066
SenpaiSilver <silvermesprims@gmail.com>
2019-03-02 00:21:34 +0100

    Fixed icons not showing.

--- application/views/base/head.php
+++ application/views/base/head.php
@@ -7,18 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <style>
-    @import url('https://fonts.googleapis.com/css?family=Lato|Major+Mono+Display|Merriweather|Montserrat|Noto+Serif+SC|Raleway|Roboto|Roboto+Condensed|Roboto+Mono|Sarabun|Source+Code+Pro');
+        @import url('https://fonts.googleapis.com/css?family=Lato|Major+Mono+Display|Merriweather|Montserrat|Noto+Serif+SC|Raleway|Roboto|Roboto+Condensed|Roboto+Mono|Sarabun|Source+Code+Pro');
 
-    @font-face {
-    font-family: 'senpaisilver-icons';
-    src:  url('../fonts/senpaisilver-icons.eot?xevsmz');
-    src:  url('../fonts/senpaisilver-icons.eot?xevsmz#iefix') format('embedded-opentype'),
-        url('../fonts/senpaisilver-icons.ttf?xevsmz') format('truetype'),
-        url('../fonts/senpaisilver-icons.woff?xevsmz') format('woff'),
-        url('../fonts/senpaisilver-icons.svg?xevsmz#icomoon') format('svg');
-    font-weight: normal;
-    font-style: normal;
-    }
+        @font-face {
+            font-family: 'senpaisilver-icons';
+            src:  url('<?=base_url("assets/fonts/senpaisilver-icons.eot?xevsmz")?>');
+            src:  url('<?=base_url("assets/fonts/senpaisilver-icons.eot?xevsmz#iefix")?>') format('embedded-opentype'),
+                url('<?=base_url("assets/fonts/senpaisilver-icons.ttf?xevsmz")?>') format('truetype'),
+                url('<?=base_url("assets/fonts/senpaisilver-icons.woff?xevsmz")?>') format('woff'),
+                url('<?=base_url("assets/fonts/senpaisilver-icons.svg?xevsmz#icomoon")?>') format('svg');
+            font-weight: normal;
+            font-style: normal;
+        }
     </style>
 
     <link href="/assets/dist/css/main.css" rel="stylesheet"/>
