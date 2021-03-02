da57809e9b33a6476ad70985689e004597f07ec3
SenpaiSilver <silvermesprims@gmail.com>
2021-03-03 00:22:08 +0100

    Missing dat trailling 0 amirite ?

--- webapp/src/BlogArticle.js
+++ webapp/src/BlogArticle.js
@@ -42,7 +42,7 @@ class BlogArticle extends React.Component {
         return (
             d.getFullYear() + "/"
                 + (d.getMonth() + 1).toString().padStart(2, "0") + "/"
-                + d.getDate() + " "
+                + d.getDate().toString().padStart(2, "0") + " "
                 + d.getHours().toString().padStart(2, "0")
                 + ":" + d.getMinutes().toString().padStart(2, "0")
         );
