d1fa4c1f5f92e414b59ef854457ec721ce00695e
SenpaiSilver <silvermesprims@gmail.com>
2023-04-30 02:57:07 +0200

    Language stays

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -11,16 +11,18 @@ interface LanguageContextProps {
     setLanguage: (lang: string) => void;
 }
 
+let detectedLanguage = localStorage.getItem("language");
+
+if (!detectedLanguage) {
+    detectedLanguage = navigator.language.split("-").at(0) || "en";
+}
+
 export default function App() {
     const translation_strings: Translation = { fr: fr_translations, en: en_translations };
-    const [currentLanguage, setCurrentLanguage] = useState<string>("en");
-    const [currentTranslations, setCurrentTranslations] = useState(translation_strings["en"]);
-
-    let detectedLanguage = localStorage.getItem("language");
-
-    if (!detectedLanguage) {
-        detectedLanguage = navigator.language.split("-").at(0) || "en";
-    }
+    const [currentLanguage, setCurrentLanguage] = useState<string>(detectedLanguage!);
+    const [currentTranslations, setCurrentTranslations] = useState(
+        translation_strings[detectedLanguage!]
+    );
 
     const value = useMemo(
         () => ({
@@ -28,11 +30,11 @@ export default function App() {
             translations: (str: string) =>
                 currentTranslations[str] ||
                 translation_strings["en"][str] ||
-                "MISSING TRANSLATIONS OMG",
+                "MISSING TRANSLATIONS OMG!!!!",
             setLanguage: (lang: string) => {
                 setCurrentLanguage(lang);
                 setCurrentTranslations(translation_strings[lang]);
-                // localStorage.setItem("language", lang);
+                localStorage.setItem("language", lang);
             },
         }),
         [{ translations: currentTranslations }]
