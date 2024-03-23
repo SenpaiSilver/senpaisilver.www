7ddde8be66c632f3986d73624b6b433048267fc4
SenpaiSilver <silvermesprims@gmail.com>
2024-03-23 18:17:40 +0100

    Updated about me

--- webapp/src/Language/fr.tsx
+++ webapp/src/Language/fr.tsx
@@ -18,11 +18,36 @@ export const fr_translations: Translation = {
                 Je produit aussi des vidéos pour partager mes avis à travers des critiques de jeux
                 et ma réflexion dans des essais vidéos.
             </p>
-
             <p>
                 J'ai <GlitchedAge /> ans et ma couleur préférée est le rouge. Je stream
                 principalement en français, mais je peux aussi streamer en anglais et j'ai des bases
                 en japonais aussi.
+            </p>
+
+            <h2>Historique</h2>
+            <h3>Le commencement</h3>
+            <p>
+                Ma toute première première vidéo sur YouTube date du 27 septembre 2006. J'ai réalisé
+                ma première vidéo scriptée en 2020.
+                <br />
+                Je considére que mon premier vrai stream a eu lieu en janvier 2020 sur YouTube, mais
+                en réalité mon avatar est apparu en 2021.
+            </p>
+            <h3>2022</h3>
+            <p>
+                En 2022 j'ai réalisé au moins une vidéo par mois dans le but de travailler
+                l'écriture de vidéos et m'initier de façon sérieuse au montage vidéo.
+            </p>
+            <h3>2023</h3>
+            <p>
+                En 2023 j'ai prit la décision de mettre plus sérieusement au stream et je suis
+                devenu affilié sur Twich le 20 mars 2023. Cette année marque aussi le début des
+                streams durant lesquels je fais du modélisme (gunpla ou plamo).
+            </p>
+            <h3>2024</h3>
+            <p>
+                En 2024 mon avatar a été mit à jour. J'ai recréé toutes les textures du visage, des
+                yeux et j'ai retravaillé un peu la tenue et les couleurs.
             </p>
         </>
     ),
