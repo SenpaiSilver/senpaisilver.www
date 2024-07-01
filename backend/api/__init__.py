78e1c878e2389b555af31d372917e2715faf99e5
SenpaiSilver <silvermesprims@gmail.com>
2024-07-01 11:48:37 +0200

    Commented out yt

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from .blog import bp as blog_bp
 from .monitoring import bp as monitoring_bp
 from .twitch import bp as twitch_bp
-from .youtube import bp as youtube_bp
+# from .youtube import bp as youtube_bp
 
 
 bp = Blueprint("api", __name__, url_prefix="/api")
@@ -11,7 +11,7 @@ blueprints = [
     blog_bp,
     monitoring_bp,
     twitch_bp,
-    youtube_bp,
+    # youtube_bp,
 ]
 for bp_module in blueprints:
     bp.register_blueprint(bp_module)
