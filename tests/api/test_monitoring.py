044a27a2c916c19092f2e079ffe4cbf2557292ca
SenpaiSilver <silvermesprims@gmail.com>
2024-09-08 15:00:58 +0200

    Fixed unit tests

--- tests/api/test_monitoring.py
+++ tests/api/test_monitoring.py
@@ -3,10 +3,11 @@ from backend.api.monitoring import heartbeat
 
 def testheartbeat():
     beat = heartbeat()
-    assert 200 == beat.status_code
-    assert b"<3" == beat.data
+    assert "<3" == beat
 
 
 def test_get_heartbeat(client):
     response = client.get("/api/monitoring/heartbeat")
     assert response is not None
+    assert 200 == response.status_code
+    assert "<3" == response.text
